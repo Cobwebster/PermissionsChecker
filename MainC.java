f3870b4fa10ca0dd37a51d7951469a702fa79220
Will <williameaster@hotmail.com>
2017-12-23 01:25:41 +0000

    Final

--- MainC.java
+++ MainC.java
@@ -1,10 +1,5 @@
 import javax.swing.JFrame;
 
-import javax.net.ssl.HttpsURLConnection;
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.net.URL;
-
 public class MainC {
 
     public static void main(String[] args) {
